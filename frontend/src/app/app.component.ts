8febe655c921dfc05e4e89e9e65733f85697f5e4
studkaraok5719 <okan.karaoglan@stud.th-rosenheim.de>
2024-04-08 19:16:04 +0200

    optimized search functionality

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -18,16 +18,20 @@ export class AppComponent {
   newForm = new FormGroup({
     fieldVal: new FormControl('')
   });
+  noDataMessage: string = "No data fetched!";
 
   constructor(private breweryService: BreweryService) { }
 
-  ngOnInit() { }
+  ngOnInit() {}
 
   onSearch() {
     let enteredCityName = this.newForm.get('fieldVal')?.value;
     this.breweryService.getBreweriesByCity(enteredCityName).subscribe((data: any) => {
           console.log(data);
           this.breweries = JSON.parse(data.data);
+          if(this.breweries.length == 0){
+            this.noDataMessage = "City not found. Try again.";
+          }
           console.log(this.breweries);
         }
       );
