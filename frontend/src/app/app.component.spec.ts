25b46555206a708ca54068d9b06b2f851b32c5b1
studkaraok5719 <okan.karaoglan@stud.th-rosenheim.de>
2024-06-12 13:10:29 +0200

    added some frontend tests

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,11 +1,26 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { BreweryService } from './services/brewery.service';
+import { of } from 'rxjs';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let breweryService: BreweryService;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [ReactiveFormsModule],
+      declarations: [AppComponent],
+      providers: [
+        { provide: BreweryService, useValue: { getBreweriesByCity: () => of([]) } }
+      ]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    breweryService = TestBed.inject(BreweryService);
   });
 
   it('should create the app', () => {
@@ -25,5 +40,36 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend');
+  });
+
+  it(`should have the 'Brewery Details' title`, () => {
+    expect(component.title).toEqual('Brewery Details');
+  });
+
+  it('should initialize breweries as an empty array', () => {
+    expect(component.breweries).toEqual([]);
+  });
+
+  it('should initialize noDataMessage correctly', () => {
+    expect(component.noDataMessage).toEqual('No data fetched, please insert a city name!');
+  });
+
+  it('should update breweries and noDataMessage correctly when onSearch is called', () => {
+    const mockBreweries = [{ name: 'Test Brewery' }];
+    spyOn(breweryService, 'getBreweriesByCity').and.returnValue(of(mockBreweries));
+
+    component.onSearch();
+
+    expect(component.breweries).toEqual(mockBreweries);
+    expect(component.noDataMessage).toEqual('');
+  });
+
+  it('should update breweries to an empty array and noDataMessage to an error message when no data is returned', () => {
+    spyOn(breweryService, 'getBreweriesByCity').and.returnValue(of(null));
+
+    component.onSearch();
+
+    expect(component.breweries).toEqual([]);
+    expect(component.noDataMessage).toEqual('City not found. Try again.');
   });
 });
--- frontend/src/app/services/brewery.service.spec.ts
+++ frontend/src/app/services/brewery.service.spec.ts
@@ -1,16 +1,38 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { BreweryService } from './brewery.service';
 
 describe('BreweryService', () => {
   let service: BreweryService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [BreweryService]
+    });
+
     service = TestBed.inject(BreweryService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should fetch breweries by city', () => {
+    const mockBreweries = [{ name: 'Test Brewery' }];
+
+    service.getBreweriesByCity('test').subscribe((breweries: any) => {
+      expect(breweries).toEqual(mockBreweries);
+    });
+
+    const req = httpMock.expectOne('https://api.openbrewerydb.org/v1/breweries');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockBreweries);
   });
 });
