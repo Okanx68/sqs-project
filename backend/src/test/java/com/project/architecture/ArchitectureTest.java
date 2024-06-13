647c5111123da9a27fc48e31429c77f575512d01
studkaraok5719 <okan.karaoglan@stud.th-rosenheim.de>
2024-06-13 15:27:08 +0200

    added/optimized ArchUnit architecture tests

--- backend/src/test/java/com/project/architecture/ArchitectureTest.java
+++ backend/src/test/java/com/project/architecture/ArchitectureTest.java
@@ -2,27 +2,54 @@ package com.project.architecture;
 
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.lang.ArchRule;
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import com.tngtech.archunit.library.Architectures;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
+
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 
 
 @AnalyzeClasses(packages = "com.project")
 class ArchitectureTest {
 
     @Test
-    void boundary_should_depend_on_controller_and_dto() {
-        JavaClasses importedClasses = new ClassFileImporter().importPackages("com.project");
+    void testLayeredArchitecture() {
+        JavaClasses importedClasses = new ClassFileImporter()
+                .withImportOption(new ImportOption.DoNotIncludeTests())
+                .importPackages("com.project");
 
-        ArchRule boundaryShouldUseController = ArchRuleDefinition.classes().that().resideInAPackage("..boundary..")
-                .and().areNotAnnotatedWith(Test.class).and().areNotAnnotatedWith(QuarkusTest.class)
+        Architectures.LayeredArchitecture layeredArchitecture = Architectures.layeredArchitecture()
+                .consideringAllDependencies()
+                .layer("Boundary").definedBy("..boundary..")
+                .layer("Controller").definedBy("..controller..")
+                .layer("Service").definedBy("..service..")
+                .layer("Entity").definedBy("..entity..")
+                .layer("DTO").definedBy("..dto..")
+                .whereLayer("Boundary").mayOnlyBeAccessedByLayers("Controller")
+                .whereLayer("Controller").mayOnlyBeAccessedByLayers("Boundary", "Service")
+                .whereLayer("Service").mayOnlyBeAccessedByLayers("Controller")
+                .whereLayer("Entity").mayOnlyBeAccessedByLayers("Controller", "Service")
+                .whereLayer("DTO").mayOnlyBeAccessedByLayers("Boundary", "Controller", "Service", "Entity");
+
+        layeredArchitecture.check(importedClasses);
+    }
+
+    @Test
+    void boundary_should_depend_on_controller() {
+        JavaClasses importedClasses = new ClassFileImporter()
+                .withImportOption(new ImportOption.DoNotIncludeTests())
+                .importPackages("com.project");
+
+        ArchRule boundaryShouldUseController = classes().that().resideInAPackage("..boundary..")
                 .should().dependOnClassesThat().resideInAPackage("..controller..")
                 .because("Boundary should depend on Controller");
 
         ArchRule boundaryShouldNotUseEntityOrService = ArchRuleDefinition.noClasses().that().resideInAPackage("..boundary..")
-                .and().areNotAnnotatedWith(Test.class).and().areNotAnnotatedWith(QuarkusTest.class)
                 .should().dependOnClassesThat().resideInAnyPackage("..entity..", "..service..")
                 .because("Boundary should not depend on Entity or Service");
 
@@ -32,25 +59,23 @@ class ArchitectureTest {
 
     @Test
     void controller_should_depend_on_entity_dto_and_service() {
-        JavaClasses importedClasses = new ClassFileImporter().importPackages("com.project");
+        JavaClasses importedClasses = new ClassFileImporter()
+                .withImportOption(new ImportOption.DoNotIncludeTests())
+                .importPackages("com.project");
 
-        ArchRule controllerShouldUseEntity = ArchRuleDefinition.classes().that().resideInAPackage("..controller..")
-                .and().areNotAnnotatedWith(Test.class).and().areNotAnnotatedWith(QuarkusTest.class)
+        ArchRule controllerShouldUseEntity = classes().that().resideInAPackage("..controller..")
                 .should().dependOnClassesThat().resideInAPackage("..entity..")
                 .because("Controller should depend on Entity");
 
-        ArchRule controllerShouldUseDTO = ArchRuleDefinition.classes().that().resideInAPackage("..controller..")
-                .and().areNotAnnotatedWith(Test.class).and().areNotAnnotatedWith(QuarkusTest.class)
+        ArchRule controllerShouldUseDTO = classes().that().resideInAPackage("..controller..")
                 .should().dependOnClassesThat().resideInAPackage("..dto..")
                 .because("Controller should depend on DTO");
 
-        ArchRule controllerShouldUseService = ArchRuleDefinition.classes().that().resideInAPackage("..controller..")
-                .and().areNotAnnotatedWith(Test.class).and().areNotAnnotatedWith(QuarkusTest.class)
+        ArchRule controllerShouldUseService = classes().that().resideInAPackage("..controller..")
                 .should().dependOnClassesThat().resideInAPackage("..service..")
                 .because("Controller should depend on Service");
 
         ArchRule controllerShouldNotUseBoundary = ArchRuleDefinition.noClasses().that().resideInAPackage("..controller..")
-                .and().areNotAnnotatedWith(Test.class).and().areNotAnnotatedWith(QuarkusTest.class)
                 .should().dependOnClassesThat().resideInAPackage("..boundary..")
                 .because("Controller should not depend on Boundary");
 
@@ -62,15 +87,15 @@ class ArchitectureTest {
 
     @Test
     void entity_should_depend_on_dto() {
-        JavaClasses importedClasses = new ClassFileImporter().importPackages("com.project");
+        JavaClasses importedClasses = new ClassFileImporter()
+                .withImportOption(new ImportOption.DoNotIncludeTests())
+                .importPackages("com.project");
 
-        ArchRule entityShouldUseDTO = ArchRuleDefinition.classes().that().resideInAPackage("..entity..")
-                .and().areNotAnnotatedWith(Test.class).and().areNotAnnotatedWith(QuarkusTest.class)
+        ArchRule entityShouldUseDTO = classes().that().resideInAPackage("..entity..")
                 .should().dependOnClassesThat().resideInAPackage("..dto..")
                 .because("Entity should depend on DTO");
 
         ArchRule entityShouldNotDependOnBoundaryControllerOrService = ArchRuleDefinition.noClasses().that().resideInAPackage("..entity..")
-                .and().areNotAnnotatedWith(Test.class).and().areNotAnnotatedWith(QuarkusTest.class)
                 .should().dependOnClassesThat().resideInAnyPackage("..boundary..", "..controller..", "..service..")
                 .because("Entity should not depend on Boundary, Controller, or Service");
 
@@ -80,10 +105,11 @@ class ArchitectureTest {
 
     @Test
     void service_should_depend_on_nothing() {
-        JavaClasses importedClasses = new ClassFileImporter().importPackages("com.project");
+        JavaClasses importedClasses = new ClassFileImporter()
+                .withImportOption(new ImportOption.DoNotIncludeTests())
+                .importPackages("com.project");
 
         ArchRule serviceShouldDependOnNothing = ArchRuleDefinition.noClasses().that().resideInAPackage("..service..")
-                .and().areNotAnnotatedWith(Test.class).and().areNotAnnotatedWith(QuarkusTest.class)
                 .should().dependOnClassesThat().resideInAnyPackage("..boundary..", "..controller..", "..entity..", "..dto..")
                 .because("Service should depend on nothing");
 
@@ -92,10 +118,11 @@ class ArchitectureTest {
 
     @Test
     void boundary_classes_should_have_specific_naming_convention() {
-        JavaClasses importedClasses = new ClassFileImporter().importPackages("com.project");
+        JavaClasses importedClasses = new ClassFileImporter()
+                .withImportOption(new ImportOption.DoNotIncludeTests())
+                .importPackages("com.project");
 
-        ArchRule rule = ArchRuleDefinition.classes().that().resideInAPackage("..boundary..")
-                .and().areNotAnnotatedWith(Test.class).and().areNotAnnotatedWith(QuarkusTest.class)
+        ArchRule rule = classes().that().resideInAPackage("..boundary..")
                 .should().haveSimpleNameEndingWith("Resource")
                 .because("Boundary classes should end with 'Resource'");
 
@@ -104,10 +131,11 @@ class ArchitectureTest {
 
     @Test
     void controller_classes_should_have_specific_naming_convention() {
-        JavaClasses importedClasses = new ClassFileImporter().importPackages("com.project");
+        JavaClasses importedClasses = new ClassFileImporter()
+                .withImportOption(new ImportOption.DoNotIncludeTests())
+                .importPackages("com.project");
 
-        ArchRule rule = ArchRuleDefinition.classes().that().resideInAPackage("..controller..")
-                .and().areNotAnnotatedWith(Test.class).and().areNotAnnotatedWith(QuarkusTest.class)
+        ArchRule rule = classes().that().resideInAPackage("..controller..")
                 .should().haveSimpleNameEndingWith("Controller")
                 .because("Controller classes should end with 'Controller'");
 
@@ -116,10 +144,11 @@ class ArchitectureTest {
 
     @Test
     void dto_classes_should_have_specific_naming_convention() {
-        JavaClasses importedClasses = new ClassFileImporter().importPackages("com.project");
+        JavaClasses importedClasses = new ClassFileImporter()
+                .withImportOption(new ImportOption.DoNotIncludeTests())
+                .importPackages("com.project");
 
-        ArchRule rule = ArchRuleDefinition.classes().that().resideInAPackage("..dto..")
-                .and().areNotAnnotatedWith(Test.class).and().areNotAnnotatedWith(QuarkusTest.class)
+        ArchRule rule = classes().that().resideInAPackage("..dto..")
                 .should().haveSimpleNameEndingWith("DTO")
                 .because("DTO classes should end with 'DTO'");
 
@@ -128,13 +157,28 @@ class ArchitectureTest {
 
     @Test
     void service_classes_should_have_specific_naming_convention() {
-        JavaClasses importedClasses = new ClassFileImporter().importPackages("com.project");
+        JavaClasses importedClasses = new ClassFileImporter()
+                .withImportOption(new ImportOption.DoNotIncludeTests())
+                .importPackages("com.project");
 
-        ArchRule rule = ArchRuleDefinition.classes().that().resideInAPackage("..service..")
-                .and().areNotAnnotatedWith(Test.class).and().areNotAnnotatedWith(QuarkusTest.class)
+        ArchRule rule = classes().that().resideInAPackage("..service..")
                 .should().haveSimpleNameEndingWith("Service")
                 .because("Service classes should end with 'Service'");
 
         rule.check(importedClasses);
     }
+
+    @Test
+    void test_classes_should_have_specific_naming_convention() {
+        JavaClasses importedClasses = new ClassFileImporter().importPackages("com.project");
+
+        ArchRule rule = classes().that()
+                .resideInAnyPackage("..architecture..", "..boundary..", "..controller..", "..entity..", "..integration..")
+                .and().areAnnotatedWith(Test.class).or().areAnnotatedWith(QuarkusTest.class)
+                .should().haveSimpleNameEndingWith("Test")
+                .because("Test classes should end with 'Test'");
+
+        rule.check(importedClasses);
+    }
+
 }
